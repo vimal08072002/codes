a23ee67597a063b36d53c76e8c2db9eb857f3d6e
vimal08072002 <135785195+vimal08072002@users.noreply.github.com>
2025-05-20 17:03:44 +0530

    Update Basic.java
    new change by v

--- Basic.java
+++ Basic.java
@@ -2,6 +2,7 @@ public class Basic {
     public static void main(String[] args) {
         System.out.println("Panda iam editing the code in github");
         System.out.println("Loki");
+        //hello
 
     }
     
